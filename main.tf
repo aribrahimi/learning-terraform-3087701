2361271a13de58adbc36e3a4c4195ca5e636f348
Alireza <55057735+aribrahimi@users.noreply.github.com>
2024-02-16 19:45:20 -0800

    initial commit

--- main.tf
+++ main.tf
@@ -15,8 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  ami           = var.instance_type
 
   tags = {
     Name = "HelloWorld"
