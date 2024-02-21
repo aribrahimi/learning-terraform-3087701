d34d1ff9d5274b212a4202461ee0d1ce3a8f64bd
Alireza <55057735+aribrahimi@users.noreply.github.com>
2024-02-20 17:01:55 -0800

    autoscalling

--- main.tf
+++ main.tf
@@ -14,16 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
@@ -82,4 +72,20 @@ module "blog_alb" {
   tags = {
     Environment = "dev"
   }
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_groups_arns = module.blog_alb.target_groups_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
